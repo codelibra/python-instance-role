e04929f1803df90946f61b30bdbf395068bebb50
Yiwen Wang <yiwenwa@amazon.com>
2021-07-14 18:26:56 -0700

    Add exception catching

--- application.py
+++ application.py
@@ -1,15 +1,20 @@
 # from package.MovieTable import create_movie_table, put_item
 import boto3
 import flask
+from botocore.exceptions import EndpointConnectionError
 
 application = flask.Flask(__name__)
 
 @application.route('/')
 def home():
-  client = boto3.client('sts')
-  identity = client.get_caller_identity()
+	client = boto3.client('sts')
+	try:
+		identity = client.get_caller_identity()
+	except EndpointConnectionError:
+		# Try again when connection denied by https://sts.amazonaws.com/
+		identity = client.get_caller_identity() 
 
-  return identity['Arn']
+	return identity['Arn']
 
 if __name__ == '__main__':
-  application.run(debug=True)
+	application.run(debug=True)
