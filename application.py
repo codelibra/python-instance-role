3622f31b09df5a1d9cecc6ee4339ad13c5a9dd7c
Yiwen Wang <yiwenwa@amazon.com>
2021-07-14 19:11:21 -0700

    Add while loop

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 # from package.MovieTable import create_movie_table, put_item
 import boto3
 import flask
+import time
 from botocore.exceptions import EndpointConnectionError
 
 application = flask.Flask(__name__)
@@ -8,13 +9,13 @@ application = flask.Flask(__name__)
 @application.route('/')
 def home():
 	client = boto3.client('sts')
-	try:
-		identity = client.get_caller_identity()
-	except EndpointConnectionError:
-		# Try again when connection denied by https://sts.amazonaws.com/
-		identity = client.get_caller_identity() 
-
-	return identity['Arn']
+	while True:
+		try:
+			identity = client.get_caller_identity()
+			return identity['Arn']
+		except EndpointConnectionError:
+			time.sleep(5)
+			continue
 
 if __name__ == '__main__':
 	application.run(debug=True)
