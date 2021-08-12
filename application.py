4fc7cba859873c27797ed2db295eb7e5f4185463
Shivendra Panicker <panicks@amazon.com>
2021-08-11 17:00:58 -0700

    Add more routes

--- application.py
+++ application.py
@@ -3,8 +3,10 @@ import boto3
 import flask
 import logging
 import time
+import os
 
 application = flask.Flask(__name__)
+MESSAGE = "HELLO, WORLD!\n"
 
 @application.route('/')
 def home():
@@ -18,6 +20,46 @@ def home():
 			logging.error("Failed to get caller identity: " + str(e))
 			time.sleep(1)
 			continue
+
+
+@application.route("/public")
+def root1():
+  hostname = "google.com"
+  response = os.system("ping -c 1 " + hostname)
+  print(response)
+  if response == 0:
+    print (hostname, 'is up!')
+  else:
+    print (hostname, 'is down!')
+
+  msg = MESSAGE + hostname
+  result = msg.encode("utf-8")
+  return result
+
+@application.route("/ip")
+def root2():
+  hostname = "192.30.255.112"
+  response = os.system("ping -c 1 " + hostname)
+  print(response)
+  if response == 0:
+    print (hostname, 'is up!')
+  else:
+    print (hostname, 'is down!')
+  msg = MESSAGE + hostname
+  result = msg.encode("utf-8")
+  return result
+
+@application.route("/aws")
+def root3():
+  client = boto3.client('sts')
+  for i in range(10):
+    try:
+      identity = client.get_caller_identity()
+      return identity['Arn']
+    except Exception as e:
+      print(e)
+      time.sleep(1)
+      continue
 
 if __name__ == '__main__':
 	application.run(debug=True)
