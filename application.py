a6df130e26195ebf3fc2be7d066744c7ff1bf53a
Shivendra Panicker <panicks@amazon.com>
2021-08-16 20:28:44 -0700

    ping google

--- application.py
+++ application.py
@@ -4,50 +4,19 @@ import flask
 import logging
 import time
 import os
+import requests
 
 application = flask.Flask(__name__)
 MESSAGE = "HELLO, WORLD!\n"
 
-@application.route('/')
-def home():
-  start = time.time()
-  client = boto3.client('sts')
-  while (time.time() - start < 300):
-    try:
-      identity = client.get_caller_identity()
-      return identity['Arn']
-    except Exception as e:
-      print(e)
-      time.sleep(5)
-      continue
-
-@application.route("/public")
+@application.route("/")
 def root1():
-  hostname = "google.com"
-  response = os.system("ping -c 1 " + hostname)
-  print(response)
-  if response == 0:
-    print (hostname, 'is up!')
-  else:
-    print (hostname, 'is down!')
+  hostname = "http://google.com"
+  response = r = requests.get(hostname)
 
   msg = MESSAGE + hostname
   result = msg.encode("utf-8")
   return result
-
-@application.route("/ip")
-def root2():
-  hostname = "192.30.255.112"
-  response = os.system("ping -c 1 " + hostname)
-  print(response)
-  if response == 0:
-    print (hostname, 'is up!')
-  else:
-    print (hostname, 'is down!')
-  msg = MESSAGE + hostname
-  result = msg.encode("utf-8")
-  return result
-
 
 if __name__ == '__main__':
 	application.run(debug=True)
